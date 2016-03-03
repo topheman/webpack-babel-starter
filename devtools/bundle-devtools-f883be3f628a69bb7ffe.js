0655e683067920d50ddf1e6db92d166bf7b94feb
Christophe Rosset <tophe@topheman.com>
2016-03-03 01:00:20 +0100

    some minor enhancements

--- devtools/bundle-devtools-f883be3f628a69bb7ffe.js
+++ devtools/bundle-devtools-f883be3f628a69bb7ffe.js
@@ -4,7 +4,7 @@
  * Webpack Babel Starter Kit
  * 
  * @version v0.1.0 - 03/03/2016
- * @revision #f8409b1 - https://github.com/topheman/webpack-babel-starter/tree/f8409b1630984a4106b578a4b0932019728900df
+ * @revision #a342f48 - https://github.com/topheman/webpack-babel-starter/tree/a342f48f4e50a7685e2db7bb10c61c3406f90418
  * @author Christophe Rosset <tophe@topheman.com> (http://labs.topheman.com/)
  * @copyright 2016(c) Christophe Rosset <tophe@topheman.com> (http://labs.topheman.com/)
  * @license MIT
@@ -71,6 +71,10 @@
 	  } else {
 	    console.info('Linter active, if you meet some problems, you can still run without linter, just set the env var LINTER=false.');
 	  }
+	} else {
+	  if (true) {
+	    console.info('Turn on the "Sources" tab of your devtools to inspect original source code - thanks to sourcemaps!');
+	  }
 	}
 	
 	/**
@@ -81,7 +85,7 @@
 	  console.info('MOCK mode');
 	}
 	
-	if (true) {
+	if (false) {
 	  console.info('You\'re on DEVTOOLS mode, you may have access to tools enhancing developer experience - off to you to choose to disable them in production ...');
 	}
 	
@@ -1307,4 +1311,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=bundle-devtools-dc202ee8ee89e7b5b89c.js.map
+//# sourceMappingURL=bundle-devtools-f883be3f628a69bb7ffe.js.map
