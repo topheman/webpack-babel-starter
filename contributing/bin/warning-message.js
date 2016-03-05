50aef84198f8c8b4ee5e395ddbd3099bbde314c0
Christophe Rosset <tophe@topheman.com>
2016-03-05 02:33:48 +0100

    docs(*): change warning message for non-contributers

--- contributing/bin/warning-message.js
+++ contributing/bin/warning-message.js
@@ -2,5 +2,5 @@ const log = require('npmlog');
 log.level = 'silly';
 
 log.warn('contributing', 'You launched a command reserved to contributers');
-log.info('contributing', 'Read more at https://github.com/topheman/webpack-babel-starter/blob/master/CONTRIBUTING.md');
+log.info('contributing', 'Read more at https://github.com/topheman/webpack-babel-starter/tree/master/contributing');
 console.log('');
